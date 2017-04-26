93fb0211944f6a88669d08909992bcd2a5daf409
AlonVIzel <alon.vizel@gmail.com>
2017-04-26 17:57:24 +0300

    connection and sending a JSon document to mongoDB

---
+++ app/src/main/java/com/example/androidbtcontrol/Treatment.java
@@ -0,0 +1,18 @@
+package com.example.androidbtcontrol;
+
+public class Treatment {
+    String name,type;
+
+    public Treatment(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
