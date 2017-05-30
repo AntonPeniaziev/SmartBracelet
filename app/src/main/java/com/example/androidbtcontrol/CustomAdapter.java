a69476d3fb0f4157c010264b8e064fa6b1006767
avizel <alon.vizel@gmail.com>
2017-05-30 20:47:18 +0300

    data is sent to web automatically, and not by pressing web button updating documents on web, and not just adding stability improvements

--- app/src/main/java/com/example/androidbtcontrol/CustomAdapter.java
+++ app/src/main/java/com/example/androidbtcontrol/CustomAdapter.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CustomAdapter extends BaseAdapter {
 
@@ -97,8 +98,10 @@ public class CustomAdapter extends BaseAdapter {
             public void onClick(View v) {
 //                SeList<Treatment> patientTreatments = data.get(position).getTreatmentsArray();
 //                new SendToMongodbTask().execute(patientTreatments);
-                Patient patient = data.get(position);
-                //new SendToMongodbTask(currActivity).execute(patient);
+                //Patient patient = data.get(position);
+                List<Patient> patient = new ArrayList<Patient>();
+                patient.add(data.get(position));
+                new SendToMongodbTask(currActivity).execute(patient);
 
             }
         });
