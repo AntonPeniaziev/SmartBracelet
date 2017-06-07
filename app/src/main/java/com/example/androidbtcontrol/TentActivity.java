7b90bdb2a29f13e06eb9cf1d3c821e6b304dee57
AntonPeniaziev <antonpeniaziev@gmail.com>
2017-06-07 19:01:54 +0300

    Patient info opens when NFC tag with mac is attached

--- app/src/main/java/com/example/androidbtcontrol/TentActivity.java
+++ app/src/main/java/com/example/androidbtcontrol/TentActivity.java
@@ -1,16 +1,27 @@
 package com.example.androidbtcontrol;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Typeface;
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import BTservice.BTservice;
 public class TentActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
@@ -22,12 +33,13 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
     ListView _listView;
     Button _refreshButton, _logoutButton;
     static public TreatmentsTable treatmentUidTranslator;
-
+    static private NfcAdapter mNfcAdapter;
+    static final String MIME_TEXT_PLAIN = "text/plain";
 
     /**
      * Initiates the list of bracelets around
      */
-    void initListOfBracelets(){
+    void initListOfBracelets() {
         _listView = (ListView) findViewById(android.R.id.list);
         _adapter = new CustomAdapter(this, R.layout.list_row);
         _listView.setAdapter(_adapter);
@@ -40,7 +52,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
     /**
      * Initiates the refresh Button located on the right top of the screen
      */
-    void initRefreshButton(){
+    void initRefreshButton() {
         Typeface army_font = Typeface.createFromAsset(getAssets(),  "fonts/Army.ttf");
         _refreshButton = (Button) findViewById(R.id.refreshBracelet);
         _refreshButton.setTypeface(army_font);
@@ -53,7 +65,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
      * Initiates the Log out Button located on the left top of the screen
      *
      */
-    void initLogOutButton(){
+    void initLogOutButton() {
         Typeface army_font = Typeface.createFromAsset(getAssets(),  "fonts/Army.ttf");
         _logoutButton = (Button) findViewById(R.id.logOut);
         setOnClickLogOut(_logoutButton);
@@ -65,7 +77,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
     /**
      * Initiates the Bluetooth service
      */
-    void initBTService(){
+    void initBTService() {
         _bTservice = new BTservice(this);
         final String doc_id =  getIntent().getStringExtra("DOC_ID");
         _bTservice.addStartDataToSendToAll("<1," + doc_id + ">");
@@ -90,6 +102,19 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
         _updateData = new UpdateData();
         _updateData.start();
         treatmentUidTranslator = new TreatmentsTable(TentActivity.this);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            if (!mNfcAdapter.isEnabled()) {
+                Toast.makeText(this, "Please enable NFC", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     /**
@@ -104,9 +129,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
         Patient item = _adapter.getItem(position);
         if (item.isConnected()) {
-            Intent intent = new Intent(TentActivity.this, PatientInfoActivity.class);
-            intent.putExtra("PATIENT_ID", item.getBtMac());
-            startActivity(intent);
+            openPatientActivityByMac(item.getBtMac());
         }
         else {
             _bTservice.connectByMac(item.getBtMac());
@@ -120,7 +143,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
      * @param clickYes
      * @param clickNo
      */
-    void dialogAlert(String message, String title, DialogInterface.OnClickListener clickYes, DialogInterface.OnClickListener clickNo){
+    void dialogAlert(String message, String title, DialogInterface.OnClickListener clickYes, DialogInterface.OnClickListener clickNo) {
         AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
         dlgAlert.setMessage(message);
         dlgAlert.setTitle(title);
@@ -154,7 +177,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
                 return;
             }
         };
-        dialogAlert(message, title,clickYes ,clickNo);
+        dialogAlert(message, title, clickYes ,clickNo);
 
         return true;
     }
@@ -172,14 +195,91 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     protected void onPause() {
+        stopForegroundDispatch(this, mNfcAdapter);
         super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleNFCIntent(intent);
+    }
+
+    /**
+     * dispatches NFC actions when tag is attached
+     * @param intent
+     */
+    private void handleNFCIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs = null;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            }
+            buildTagViews(msgs);
+        }
+    }
+    /**
+     * fetches NDEF messages headers
+     * @param msgs
+     */
+    private void buildTagViews(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) return;
+
+        String text = "";
+//        String tagId = new String(msgs[0].getRecords()[0].getType());
+        byte[] payload = msgs[0].getRecords()[0].getPayload();
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16"; // Get the Text Encoding
+        int languageCodeLength = payload[0] & 0063; // Get the Language Code, e.g. "en"
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+
+        try {
+            // Get the Text
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+
+        openPatientActivityByMac(text);
+    }
+
+    /**
+     * Starts new PtientActivity
+     * @param mac - patient's bracelet mac-address
+     */
+    private void openPatientActivityByMac(String mac) {
+        if (false == _bTservice.isConnectedToBtMac(mac)) {
+            Toast.makeText(this,
+                    "Unknown device address\n",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(TentActivity.this, PatientInfoActivity.class);
+        intent.putExtra("PATIENT_ID", mac);
+        startActivity(intent);
+    }
 
     /**
      * updates the data from web and Bluetooth
@@ -214,7 +314,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
      * updates the list of bracelets
      * @param data
      */
-    void updateListView(ArrayList<Patient> data){
+    void updateListView(ArrayList<Patient> data) {
         _adapter.setData(data);
         _adapter.notifyDataSetChanged();
     }
@@ -241,7 +341,7 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
      * @return
      */
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)  {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (Integer.parseInt(android.os.Build.VERSION.SDK) > 5
                 && keyCode == KeyEvent.KEYCODE_BACK
                 && event.getRepeatCount() == 0) {
@@ -261,7 +361,8 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
         String title = "Smart Bracelet";
         DialogInterface.OnClickListener clickYes = new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                System.exit(0);
+                LoginActivity._loginButton.setEnabled(true);
+                finish();
             }
         };
 
@@ -335,6 +436,39 @@ public class TentActivity extends AppCompatActivity implements AdapterView.OnIte
         return _tent.getTreatmentsArrayByMac(patientMac);
     }
 
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding equesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
 }
 
 
