7fa2aa46f713394eb9d451eef0ffacb24619d851
avizel <alon.vizel@gmail.com>
2017-06-19 19:38:19 +0300

    DB names changed

--- app/src/main/java/tasks/SendToMongodbTask.java
+++ app/src/main/java/tasks/SendToMongodbTask.java
@@ -65,20 +65,20 @@ public class SendToMongodbTask extends AsyncTask<ArrayList<Patient>, Integer, Bo
                 treatList.add(treatDoc);
             }
 
-            document.put("bracelet_id", patient.getBtMac());
+            document.put("Bracelet_ID", patient.getBtMac());
             document.put("heart_rate", patient.getHeartRate());
             document.put("breathe_rate", patient.getBreatheRate());
             document.put("blood_pressure", patient.getBloodPressure());
             document.put("body_temp", patient.getBodyTemp());
-            document.put("evacuation_request", String.valueOf(patient.is_urgentEvacuation()));
-            document.put("injury_stat", patient.getPatientState());
-            document.put("doctor_name", LoginActivity.doctorName);
-            document.put("doctor_number", LoginActivity.doctorNumber);
-            document.put("longitude", TentActivity.locationListener.getLongitude());
-            document.put("latitude", TentActivity.locationListener.getLatitude());
+            document.put("evacuation_request", String.valueOf(patient.getUrgentEvacuationState()));
+            document.put("Status", patient.getPatientState());
+            document.put("Dr_Name", LoginActivity.doctorName);
+            document.put("Dr_number", LoginActivity.doctorNumber);
+            document.put("Longitude", TentActivity.locationListener.getLongitude());
+            document.put("Latitude", TentActivity.locationListener.getLatitude());
             document.put("treatments", treatList);
 
-            Bson searchQuery = new Document("bracelet_id", patient.getBtMac());
+            Bson searchQuery = new Document("Bracelet_ID", patient.getBtMac());
             UpdateOptions upsertDoc = new UpdateOptions();
             upsertDoc.upsert(true);
 
